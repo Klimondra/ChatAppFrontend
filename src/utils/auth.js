2309b92017957d23bf854ff70b447f4b002e8b3c
Klimondra <klimondra@seznam.cz>
2025-07-15 14:06:26 +0200

    SSL Fixed
    Took 6 minutes

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -11,6 +11,9 @@ const pool = new Pool({
     max: 20,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 })
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
